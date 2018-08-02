005bfe710f7997fa7f79880542dedd8db873b3c2
alexandracch <alexa_theprinces@hotmail.com>
2018-08-02 09:57:42 -0500

    cambios

--- nav.js
+++ nav.js
@@ -5,3 +5,23 @@ $(document).ready(function(){
   $(document).ready(function(){
     $('select').formSelect();
   });
+
+ // Botón
+
+ // Selección de elementos
+ var $button = $('#myButton');
+ var $select = $('select');
+ var $inputAltura = $('input-altura');
+ var $inputPeso = $('input-peso');
+
+
+ $button.on('click',function(){
+     console.log('hola')
+ });
+
+
+
+
+
+
+ 
