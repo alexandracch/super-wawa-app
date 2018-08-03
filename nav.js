89246c346dfd56913eb2fc081dbd99316c25be02
alexandracch <alexa_theprinces@hotmail.com>
2018-08-03 10:25:29 -0500

    js

--- nav.js
+++ nav.js
@@ -11,17 +11,34 @@ $(document).ready(function(){
  // Selección de elementos
  var $button = $('#myButton');
  var $select = $('select');
- var $inputAltura = $('input-altura');
+ var $inputAltura = $('input[id=input-altura]').val();
  var $inputPeso = $('input-peso');
-
+ var $inputFecha = $('input[id=fecha]').val();
 
  $button.on('click',function(){
      console.log('hola')
  });
 
+$(document).ready(function(){
+  
+  $('#sexo-wawa').change(function() {
+             console.log($('select[id=sexo-wawa]').val());
+            //  $('#fem').val($(this).val());
 
+            //  var $dataSelect =;
 
+             if ( $('select[id=sexo-wawa]').val() >= 2) {
+              console.log('pasa a función de altura para niños');
+              
+            } else {
+              console.log('pasa a función de altura para niñas')
+            }
+  })});
 
+  if ($inputFecha >= 2013-01-01) {
+    console.log('tiene ')
+  }
 
-
- 
+// function alturaNiñas () {
+//   if ($inputAltura > 0)
+// }
